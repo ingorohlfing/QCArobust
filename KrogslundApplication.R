c44cd196502cfb3188c5d3d3398b576fec6a068b
Jeren Rozyjumayeva <rozyjumayeva@wiso.uni-koeln.de>
2019-05-02 15:08:40 +0200

    Krogslund

---
+++ KrogslundApplication.R
@@ -0,0 +1,37 @@
+#Krogslund Application
+#Preliminaries and libraries
+rm(list = ls())
+library(QCAviz)
+library(dplyr)
+
+#data import (computer-specific modifications are required)
+ka_model_1_ncut1_error <- read_csv("~/ka_model_1_ncut1_error.csv")
+ka_model_1_sim <- read_csv("~/ka_model_1_sim.csv")
+model_2_ncut1_error <- read_csv("~/model_2_ncut1_error.csv")
+model_2_sim <- read_csv("~/model_2_sim.csv")
+
+#Applying the functions
+#the first dataset
+conds_upset(ka_model_1_ncut1_error$Configurations, nsets = 100)
+config_upset(ka_model_1_ncut1_error$Configurations, nsets = 50)
+sols_upset(ka_model_1_ncut1_error$Configurations, nsets = 9)
+
+#the second dataset
+conds_upset(ka_model_1_sim$Configurations, nsets = 70)
+config_upset(ka_model_1_sim$Configurations, nsets = 50)
+sols_upset(ka_model_1_sim$Configurations, nsets = 9)
+
+#the third dataset
+conds_upset(model_2_ncut1_error$Configurations, nsets = 70)
+config_upset(model_2_ncut1_error$Configurations, nsets = 50)
+sols_upset(model_2_ncut1_error$Configurations, nsets = 9)
+
+#the fourth dataset
+conds_upset(model_2_sim$Configurations, nsets = 70)
+config_upset(model_2_sim$Configurations, nsets = 50)
+sols_upset(model_2_sim$Configurations, nsets = 9)
+
+
+
+
+
