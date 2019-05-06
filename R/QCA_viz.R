c4b26524c6d8a31be2f8a0569384969c8b649b84
Jeren Rozyjumayeva <rozyjumayeva@wiso.uni-koeln.de>
2019-05-06 17:25:27 +0200

    the first config rewriting

--- R/QCA_viz.R
+++ R/QCA_viz.R
@@ -247,3 +247,24 @@ list.selector <- function(x, con.thresh = 0){
 
   return(output)
 }
+
+#the config_functions
+config_upset_1 <- functions(x, y, nsets){
+  temp1 <- dt.selector(x, con.thresh = y)
+  temp1 <- purrr::map(temp1$config, unlist)
+  temp1 <- purrr::map(temp1, function(x) stringi::stri_trim(x))
+  all_values <- stringi::stri_unique(unlist(temp1))
+  all_values <- purrr::map(all_values, function(x) stringi::stri_trim(x))
+  final_matrix <- plyr::ldply(temp1, function(y) comparison(x = all_values, y = y, num = T))
+  colnames(final_matrix) <- all_values
+  UpSetR::upset(final_matrix, order.by = "freq", nsets = nsets)
+}
+
+
+
+
+
+
+
+
+
