0d2249066a4115a56d6e3af972ca09071d1b5a65
AyjerenR <jeren.jmv@gmail.com>
2019-05-28 17:25:09 +0200

    Trying to incorporate the datasets inside the package.

--- R/QCA_viz.R
+++ R/QCA_viz.R
@@ -295,3 +295,29 @@ barplot <- function(ls){
   return(plot2)
 }
 
+
+#Datasets Documentation ----
+
+#' hinter
+#' Data borrowed from Hinter and Leitner study
+#' A dataset containing informaiton about 20 countres
+#' used to understand the politic sof external
+#' approval: Explaining the IMF's evaluation
+#' of austerity programmes in those countries.
+#'
+#' @format A list with 20 observations of 10 variables
+#' \describe{
+#'   \item{case}{the case covered in the study. The country being analyzed}
+#'   \item{pos}{the numeric value of pos}
+#'   \item{reduction}{the numeric value of reduction}
+#'   \item{duration}{the numeric value of duration}
+#'   \item{ambition}{the numeric value of ambition}
+#'   \item{DC}{the numeric value of DC}
+#'   \item{EFF}{the numeric value of EFF}
+#'   \item{CR}{the numeric value of CR}
+#'   \item{COM}{the numeric value of COM}
+#'   \item{PRG}{the numeric value of PRG}
+#' }
+#' @source \url{http://onlinelibrary.wiley.com/doi/10.1111/1475-6765.12142/suppinfo/}
+"hinter"
+
