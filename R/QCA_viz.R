b422dcc8606fe0d78b822843ca16f49c96a9151c
AyjerenR <jeren.jmv@gmail.com>
2019-10-22 13:19:53 +0200

    extras cutting continuted.

--- R/QCA_viz.R
+++ R/QCA_viz.R
@@ -192,47 +192,6 @@ config_upset <- function(df, const = FALSE, y, nsets) {
 
 
 
-
-#' config_upset_sim()
-#'
-#' Function decomposes results into individual configurations
-#' of the output of the QCASummary function
-#'
-#' @importFrom magrittr %>%
-#' @import stringi
-#' @param df output extracted from the \pkg{QCA} package
-#' in their entirety.
-#' @param const is the argument of a function with binary
-#' input. The default option - FALSE - indicates that all
-#' of the QCA configurations will be plotted. If the argument
-#' is set to TRUE, the function will only plot those
-#' configurations which are above a chosen threshold: see
-#' the following argument for threshold.
-#' @param y is an argument with default value of zero. If
-#' any other number is set for the given argument, only
-#' those configurations which are above the set value for y
-#' will be plotted by the function
-#' @param nsets An argument imported from the \pkg{UpSetR}
-#' package. Determines the number of sets to be plotted.
-#' @return An \pkg{UpSetR}-generated intersection plot
-#' presenting the frequency of individual configurations across
-#' QCA solutions and their intersections across solutions.
-#'
-#' @export
-config_upset_sim <- function(df, nsets) {
-  temp1 <- purrr::map(df, function(x) stringi::stri_c_list(x, sep = "+")) %>% unlist()
-  temp1 <- purrr::map(temp1, function(x) stringi::stri_trim(x))
-  all_values <- stringi::stri_unique(unlist(temp1))
-  finl <- detection(temp1, all_values)
-  colnames(finl) <- paste0("sol_", 1:length(all_values))
-  UpSetR::upset(finl, order.by = "freq", nsets = nsets)
-}
-
-
-
-
-
-
 #'an original function
 #'
 #' @param list the data from within which we want to count
