d41d812ac1cfd2927718370ea6217cb5b36a538d
Ingo Rohlfing <i.rohlfing@uni-koeln.de>
2020-06-01 09:23:50 +0200

    Updated documentation and function

--- R/cons_minimum.R
+++ R/cons_minimum.R
@@ -1,28 +1,29 @@
-#' Preselecting configurations for analysis bsaed on minimum consistency value
+#' Filtering configurations from results using minimum consistency value
 #'
 #' @importFrom magrittr %>%
-#' @import stringi
+#' @importFrom stringi stri_split_fixed
 #' @import rlist
-#' @import purrr
-#' @param x output of the QCA in its entirety
-#' produced by \pkg{QCA} package
-#' @param con.thresh Minimum consistency threshold for being included in
-#' the analysis. Default is zero.
+#' @importFrom purrr map
+#' @importFrom dplyr select filter
 #'
-#' @return A list of a subset of configurations meeting the threshold.
+#' @param x List of solutions generated with \code{\link{QCA}} package
+#' @param consthresh Minimum consistency threshold for for filtering
+#' configurations. Default is zero.
+#'
+#' @return A list of configurations meeting the consistency threshold.
 #'
 #' @export
-cons_minimum <- function(x, conthresh = 0) {
+cons_minimum <- function(x, consthresh = 0) {
 
   sol <- purrr::map(x$solution, function(x) stringi::stri_split_fixed(x, "+"))
   cnst <- x$IC$incl.cov[["inclS"]]
 
   output <- rlist::list.append(sol, cnst)
   output <- rlist::list.cbind(output)
-  colnames(output) <- c("config", "consist")
+  colnames(output) <- c("config", "consistency")
 
   output <- as.data.frame(output)
-  output <- output %>% filter(consist > conthresh) %>% select(config)
+  output <- output %>% filter(consistency > consthresh) %>% select(config)
 
   return(output)
 }
