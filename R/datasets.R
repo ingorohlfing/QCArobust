086e197db276ea7ae86e39db6e759cba8685674c
Ingo Rohlfing <i.rohlfing@uni-koeln.de>
2020-10-08 19:57:19 +0200

    Correct typo in Krogslund 2015 entry

--- R/datasets.R
+++ R/datasets.R
@@ -39,7 +39,7 @@
 #'   \item{Configurations}{Configurations vector}
 #' }
 #' @source \url{https://www.cambridge.org/core/journals/political-analysis/article/fuzzy-sets-on-shaky-ground-parameter-sensitivity-and-confirmation-bias-in-fsqca/B693F136C5158DF3C19686EC89522C23}
-"krogslund"
+"Krogslund"
 
 
 #' Skaaning dataset
