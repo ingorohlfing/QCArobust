72dd824eb5af444aada8c8c6f5525892805d1a2a
Ingo Rohlfing <i.rohlfing@uni-koeln.de>
2020-10-17 23:33:10 +0200

    Update function and documentation

--- R/sols_plot.R
+++ R/sols_plot.R
@@ -9,12 +9,22 @@
 #' derived from multiple truth table analyses performed
 #' with \code{\link[QCA]{minimize}} from \pkg{QCA}
 #' package.
+#' @param plot If set to \code{TRUE}, a bar chart of frequency
+#' of solutions is produced. If set to \code{FALSE},
+#' a dataframe with solutions and frequencies is returned.
+#' Default is \code{TRUE}.
+#' @param plot_solutions Number of solutions to be plotted in
+#' bar chart. Default is 5. If \code{plot} is set to \code{FALSE},
+#' the dataframe shows all solutions regardless of the number
+#' specified for plotting.
 #'
-#' @return A bar chart created with \code{\link{ggplot2}} summarizing
-#' the frequency of solutions in the data.
+#' @return If \code{plot} set to \code{TRUE}, a bar chart of
+#' frequency of solutions is produced with \code{\link{ggplot2}}.
+#' If \code{plot} is set to \code{FALSE}, a dataframe with
+#' solutions and frequencies.
 #'
 #' @export
-sols_plot <- function(ls) {
+sols_plot <- function(ls, plot = T, plot_solutions = 5) {
 
   # preprocessing the list
   temp3 <- purrr::map(ls, function(x) stringi::stri_split_fixed(x, " "))
@@ -43,14 +53,23 @@ sols_plot <- function(ls) {
     dplyr::mutate(solution = 1:length(tableX$Raw)) %>%
     dplyr::mutate(solution = as.factor(solution)) %>%
     dplyr::mutate(solution = forcats::fct_reorder(solution, Freq, .desc = TRUE))
+  # returning dataframe
+  if(plot == F) {
+    return(newdf)
+  }
+  else {
+    # subsetting dataframe to specified number of solutions
+    newdf <- newdf[1:plot_solutions, ]
+    # removing rows if plot_solutions > number of solutions in data
+    newdf <- na.omit(newdf)
+    # creating rotated bar chart
+    plot2 <- ggplot2::ggplot(data = newdf,
+                             ggplot2::aes(x = solution, y = Freq)) +
+      ggplot2::geom_bar(stat = "identity") +
+      coord_flip() + theme_minimal() +
+      scale_x_discrete("Solution ID") + scale_y_continuous("Frequency") +
+      ggplot2::ggtitle("Frequency of solutions")
 
-  # creating rotated bar chart
-  plot2 <- ggplot2::ggplot(data = newdf,
-                           ggplot2::aes(x = solution, y = Freq)) +
-    ggplot2::geom_bar(stat = "identity") +
-    coord_flip() + theme_minimal() +
-    scale_x_discrete("Solution ID") + scale_y_continuous("Frequency") +
-    ggplot2::ggtitle("Frequency of solutions")
-
-  return(plot2)
+    return(plot2)
+  }
 }
