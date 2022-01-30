26987bde7fef1258e870eab4fe13aec86464121f
Ingo Rohlfing <i.rohlfing@uni-koeln.de>
2022-01-30 23:06:52 +0100

    Clean data description

--- R/data.R
+++ R/data.R
@@ -8,25 +8,6 @@
 #' \code{\link{QCA}} package.
 "solutions_example"
 
-#' Krogslund dataset (model 1, ncut 1)
-#' Data borrowed from Krogslund/Choi/Poertner study
-#' A dataset containing informaiton about 5 variables
-#' that are the result of a simulation analysis. This
-#' dataset refers to a simulation analysis of original data
-#' by Koenig-Archibugi.
-#'
-#' @format A tibble with 40000 observations and 5 variables
-#' \describe{
-#'   \item{incl.cut1.val}{First inclusion cut}
-#'   \item{incl.cut0.val}{Second inclusion cut}
-#'   \item{n.cut.val}{the numeric value}
-#'   \item{error}{the error}
-#'   \item{Configurations}{Configurations vector}
-#' }
-#' @source \url{https://doi.org/10.1093/pan/mpu016}
-"KCP_model1_ncut1"
-
-
 #' Skaaning dataset
 #' Data is borrowed from the robustness analysis by Skaaning and Sven-Erik (2011)
 #' The list of solutions presented in table 3 of the article. It  summarizes
