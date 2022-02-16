b4f6a0da82bf7221bfda6c57750f0eae0d84a004
ingorohlfing <i.rohlfing@uni-koeln.de>
2022-02-16 12:47:51 +0100

    Update solutions_robust

--- R/solutions_robust.R
+++ R/solutions_robust.R
@@ -36,61 +36,68 @@
 #'
 #' @export
 solutions_robust <- function(ls, plot = TRUE, plot_solutions = 5) {
-
-  # preprocessing the list
-  temp3 <- purrr::map(ls, function(x)
-    suppressWarnings(stringi::stri_split_fixed(x, " ")))
-
-  #adding the space between special characters
-  temp3 <- lapply(unlist(temp3), FUN = function(t) gsub(pattern = "[*]",
-                                                        replacement = " * ",
-                                                        x = t))
-
-
-  solutions1 <- Reduce(c, ls) %>% as.list()
-  all_values3 <- unlist(unique(solutions1))
-
-  # new function to paste the strings
-  p <- function(..., sep = ' + ') {
-    paste(..., sep = sep, collapse = sep)
+  if (class(ls) != "list") {
+    stop('The object is not a list. Function only processes lists.')
   }
-
-  # applying the new function to my list
-  j <- purrr::map(temp3, p)
-
-  # binding the solutions into a dataframe
-  b <- suppressWarnings(purrr::map_df(j,
-                                      ~ data.frame(Content = .x),
-                                      .id = "Model"
-  ))
-
-  # processing dataframe for outputting and plotting
-  tableX <- as.data.frame(table(b$Content))
-  colnames(tableX) <- c("Model", "Frequency")
-  newdf <- tableX %>%
-    dplyr::arrange(desc(Frequency)) %>%
-    dplyr::mutate(solutionid = 1:length(tableX$Model)) %>%
-    dplyr::mutate(solutionid = as.factor(solutionid)) %>%
-    dplyr::mutate(solutionid = forcats::fct_reorder(solutionid, Frequency,
-                                                    .desc = TRUE)) %>%
-    dplyr::rename(`Model ID` = solutionid)
-  # returning dataframe
-  if(plot == FALSE) {
-    return(newdf)
+  if (length(ls) == 1) {
+    stop('One needs at least two QCA models to use the function.')
   }
   else {
-    # subsetting dataframe to specified number of solutions
-    newdf <- newdf[1:plot_solutions, ]
-    # removing rows if plot_solutions > number of solutions in data
-    newdf <- na.omit(newdf)
-    # creating rotated bar chart
-    plot2 <- ggplot2::ggplot(data = newdf,
-                             ggplot2::aes(x = `Model ID`, y = Frequency)) +
-      ggplot2::geom_bar(stat = "identity") +
-      coord_flip() + theme_minimal() +
-      scale_x_discrete("Model ID") + scale_y_continuous("Frequency") +
-      ggplot2::ggtitle("Frequency of models")
+    # preprocessing the list
+    temp3 <- purrr::map(ls, function(x)
+      suppressWarnings(stringi::stri_split_fixed(x, " ")))
 
-    return(plot2)
+    #adding the space between special characters
+    temp3 <- lapply(unlist(temp3), FUN = function(t) gsub(pattern = "[*]",
+                                                          replacement = " * ",
+                                                          x = t))
+
+
+    solutions1 <- Reduce(c, ls) %>% as.list()
+    all_values3 <- unlist(unique(solutions1))
+
+    # new function to paste the strings
+    p <- function(..., sep = ' + ') {
+      paste(..., sep = sep, collapse = sep)
+    }
+
+    # applying the new function to my list
+    j <- purrr::map(temp3, p)
+
+    # binding the solutions into a dataframe
+    b <- suppressWarnings(purrr::map_df(j,
+                                        ~ data.frame(Content = .x),
+                                        .id = "Model"
+    ))
+
+    # processing dataframe for outputting and plotting
+    tableX <- as.data.frame(table(b$Content))
+    colnames(tableX) <- c("Model", "Frequency")
+    newdf <- tableX %>%
+      dplyr::arrange(desc(Frequency)) %>%
+      dplyr::mutate(solutionid = 1:length(tableX$Model)) %>%
+      dplyr::mutate(solutionid = as.factor(solutionid)) %>%
+      dplyr::mutate(solutionid = forcats::fct_reorder(solutionid, Frequency,
+                                                      .desc = TRUE)) %>%
+      dplyr::rename(`Model ID` = solutionid)
+    # returning dataframe
+    if(plot == FALSE) {
+      return(newdf)
+    }
+    else {
+      # subsetting dataframe to specified number of solutions
+      newdf <- newdf[1:plot_solutions, ]
+      # removing rows if plot_solutions > number of solutions in data
+      newdf <- na.omit(newdf)
+      # creating rotated bar chart
+      plot2 <- ggplot2::ggplot(data = newdf,
+                               ggplot2::aes(x = `Model ID`, y = Frequency)) +
+        ggplot2::geom_bar(stat = "identity") +
+        coord_flip() + theme_minimal() +
+        scale_x_discrete("Model ID") + scale_y_continuous("Frequency") +
+        ggplot2::ggtitle("Frequency of models")
+
+      return(plot2)
+    }
   }
 }
